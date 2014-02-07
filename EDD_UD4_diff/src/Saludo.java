02b7e775b984e90d5a0e4175d36d19acc0ff24e3
rubenciclos <ruben.ciclos.informatica@gmail.com>
2014-02-07 20:29:19 +0100

    Primera subida del proyecto

---
+++ EDD_UD4_diff/src/Saludo.java
@@ -0,0 +1,14 @@
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		System.out.println("Hola mundo!");
+		System.out.println("Adios mundo!");
+
+	}
+
+}
---
+++ EDD_UD4_diff/src/Saludo_2.java
@@ -0,0 +1,8 @@
+
+public class Saludo_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+	}
+
+}
